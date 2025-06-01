14259e17b992fbb5705955d33502498150714c4e
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-06-01 18:47:18 -0400

    Se agregan testing al cd

--- microservice-authentication/src/main/java/com/microservice/authentication/dto/RegisterRequest.java
+++ microservice-authentication/src/main/java/com/microservice/authentication/dto/RegisterRequest.java
@@ -1,8 +1,10 @@
 package com.microservice.authentication.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class RegisterRequest {
     private String username;
     private String password;
