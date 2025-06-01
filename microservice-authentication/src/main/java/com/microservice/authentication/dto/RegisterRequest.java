bc20c02b60bc22b653488b3652aa4f0889c1930b
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-06-01 18:04:52 -0400

    Cambios Wenos

---
+++ microservice-authentication/src/main/java/com/microservice/authentication/dto/RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.microservice.authentication.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String birthDate;
+    private String region;
+    private String commune;
+    private String address;
+}
